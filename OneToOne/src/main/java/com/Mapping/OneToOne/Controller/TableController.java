15c2826e0a37157696372891fad6107c0ee350e4
Ashish Kumar Jha <aj310161@gmail.com>
2021-08-04 00:45:32 +0530

    OneToOne Mapping Using Spring Boot Annotation

--- OneToOne/src/main/java/com/Mapping/OneToOne/Controller/TableController.java
+++ OneToOne/src/main/java/com/Mapping/OneToOne/Controller/TableController.java
@@ -1,14 +1,12 @@
 package com.Mapping.OneToOne.Controller;
 
+import com.Mapping.OneToOne.Dao.Table1Dao;
 import com.Mapping.OneToOne.Entity.Table1;
 import com.Mapping.OneToOne.Entity.Table2;
 import com.Mapping.OneToOne.Service.TableImpl;
 import com.Mapping.OneToOne.Service.TableInter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TableController {
@@ -18,5 +16,21 @@ public class TableController {
 @PostMapping("/AddData")
     public Table1 AddData(@RequestBody Table1 tabobj) {
     return tableInter.addData(tabobj);
+}
+
+@Autowired
+    Table1Dao table1Dao;
+@DeleteMapping("/delete/{id}")
+    public String deletedata(@PathVariable int id){
+    Table1 t=null;
+    for(Table1 t1: table1Dao.findAll()){
+        if(t1.getId()==id){
+            t=t1;
+            break;
+        }
+    }
+    table1Dao.delete(t);
+    return "deleted";
+
 }
 }
